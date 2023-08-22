838854615ef8fc1d5bf20b6404d8ba1c87cac74f
Addonay <addonay.osoro@student.moringaschool.com>
2023-08-22 18:32:02 +0300

    solved lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,25 @@
 def badge_maker(name):
-    return None
+    return "Hello, my name is {}.".format(name)
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
 
-def assign_rooms(names):
-    return None
+def assign_rooms(speakers):
+    assignments = []
+    for index, speaker in enumerate(speakers, start=1):
+        assignments.append("Hello, {}! You'll be assigned to room {}!".format(speaker, index))
+    return assignments
 
-def printer(names):
-    return None
+def printer(speakers):
+    badges = batch_badge_creator(speakers)
+    assignments = assign_rooms(speakers)
+    
+    for badge in badges:
+        print(badge)
+    
+    for assignment in assignments:
+        print(assignment)
+
+# Example usage
+speakers_list = ["Arel", "Carol", "John", "Emma"]
+printer(speakers_list)
